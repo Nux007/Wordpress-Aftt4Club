07bad760eced2e7163d98aadbb2103b110e094c1
Nux007 <007.nux@gmail.com>
2018-09-25 14:17:04 +0200

    Fixed commments.

--- api/index.php
+++ api/index.php
@@ -1,20 +1,20 @@
 <?php
 /**
-Aftt4Club is a wordpress plugin that helps to manage you Table Tennis club. 
-Copyright (C) 2018  Nux007
-    
-This file is part of Aftt4Club wordpress plugin.
-    
-Aftt4Club is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-    
-Aftt4Club is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-    
-You should have received a copy of the GNU General Public License
-along with Aftt4Club. If not, see <http://www.gnu.org/licenses/>.
+* Aftt4Club is a wordpress plugin that helps to manage you Table Tennis club. 
+* Copyright (C) 2018  Nux007
+*    
+* This file is part of Aftt4Club wordpress plugin.
+*    
+* Aftt4Club is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*    
+* Aftt4Club is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*    
+* You should have received a copy of the GNU General Public License
+* along with Aftt4Club. If not, see <http://www.gnu.org/licenses/>.
 **/
